88868a14ea3adb9a6037420bd39ffcdaefdab8f4
Joseph James Weitzel <joe@box.biz>
2016-02-18 17:42:35 -0500

    Update jQuery.iPhoneInlineVideo.js

--- jQuery.iPhoneInlineVideo.js
+++ jQuery.iPhoneInlineVideo.js
@@ -37,7 +37,7 @@
 						var time = Date.now(),
 							elapsed = (time - lastTime) / 1000;
 						if(!renderable && video.videoHeight && canvas.height) {
-							canvasContext.scale(1, 1);
+							//canvasContext.scale(1, 1);
 							//canvasContext.scale(1, canvas.height / video.videoHeight);
 							renderable = $canvas.animate({
 								opacity: 1.0
